15ff8a07fc007997276494346ef0a648e15f3bb0
musch038 <musch.sam@gmail.com>
2021-09-25 14:35:00 -0500

    Changes

---
+++ env.Rprofile.R
@@ -0,0 +1,13 @@
+Sys.setenv(FRED_API_KEY="07bccc6cd5cd1018027266ce854705db")
+
+Sys.setenv(CENSUS_KEY="1037078fc76765e282541656ed1b89d7cab500c5")
+
+#eia_set_key("215bf919f392786f2ebb754dd29d895b")
+Sys.setenv(eia_set_key="215bf919f392786f2ebb754dd29d895b")
+
+Sys.setenv("DATAVERSE_KEY" = "493de3c4-dfed-458a-a4f5-334c27c406f3")
+Sys.setenv("DATAVERSE_SERVER" = "dataverse.harvard.edu")
+
+#beaKey = '87FC75DA-90CD-4A77-B3EB-9A2A7932C213'
+Sys.setenv(beaKey = "87FC75DA-90CD-4A77-B3EB-9A2A7932C213")
+# bls eb2f1580c1a141a2b0d01a15903f99c7
