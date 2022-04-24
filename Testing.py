5de2ccce569ae5a05d1cb352314aebe565b3d285
musch038 <musch.sam@gmail.com>
2022-04-24 13:37:32 -0500

    comment from python script

--- Testing.py
+++ Testing.py
@@ -2,13 +2,18 @@ from git import Repo
 import pandas as pd
 import schedule
 import time
+import nasdaqdatalink
 
 PATH_OF_GIT_REPO = r'/Users/Sam/01-Economics'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'comment from python script'
 
 def git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE):
-    df = pd.DataFrame({'a': [1,2],'b': [3,4]})
+    cpi = nasdaqdatalink.get("FRED/CPIAUCSL").reset_index().tail(40).rename(columns={'Value':'CPI'})
+    unemployment = nasdaqdatalink.get("FRED/UNRATE").reset_index().tail(40).rename(columns={'Value':'Unemployment'})
+    real_disposable = nasdaqdatalink.get("FRED/DSPIC96").reset_index().tail(40).rename(columns={'Value':'Real_Dispose'})
+    df = cpi.merge(unemployment, how='outer', on='Date').merge(real_disposable, how='outer', on='Date')
     df.to_csv('/Users/Sam/01-Economics/CSV Files/Macro_Vars.csv')    
+    print(df)
     
     try:
         repo = Repo(PATH_OF_GIT_REPO)
@@ -19,4 +24,9 @@ def git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE):
     except:
         print('Some error occured while pushing the code')    
 
-schedule.every().minute.do(git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE))
+git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE)
+
+
+#def job():
+#schedule.every().minute.do(job)
+
--- Testing2.py
+++
@@ -1,9 +0,0 @@
-import schedule
-import time
-
-def job():
-    print("I'm working...")
- 
-schedule.every().minute.do(job)
-    
-#schedule.every().day.at("10:30").do(job)
