0b15053d9e5825966678c73f22fae798d338b16a
musch038 <musch.sam@gmail.com>
2022-04-24 12:31:27 -0500

    comment from python script

---
+++ Testing.py
@@ -0,0 +1,25 @@
+from git import Repo
+import pandas as pd
+
+df = pd.DataFrame({
+    'a': [1,2],
+    'b': [3,4]
+})
+
+df.to_csv('testing.csv')
+
+PATH_OF_GIT_REPO = r'/Users/Sam/01-Economics'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'comment from python script'
+
+def git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE):
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(all=True) # , update=True
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')    
+
+git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE)
+
