d637988d7d7cc07429bcbf250ded9a9d4ec299ba
musch038 <musch.sam@gmail.com>
2022-05-15 09:52:23 -0500

    comment from python script

--- Testing.py
+++ Testing.py
@@ -2,17 +2,25 @@ from git import Repo
 import pandas as pd
 import schedule
 import time
-import nasdaqdatalink
+#import nasdaqdatalink
+from fredapi import Fred
+fred = Fred(api_key='07bccc6cd5cd1018027266ce854705db')
 
 PATH_OF_GIT_REPO = r'/Users/Sam/01-Economics'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'comment from python script'
 
 def git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE):
-    cpi = nasdaqdatalink.get("FRED/CPIAUCSL").reset_index().tail(40).rename(columns={'Value':'CPI'})
-    unemployment = nasdaqdatalink.get("FRED/UNRATE").reset_index().tail(40).rename(columns={'Value':'Unemployment'})
-    real_disposable = nasdaqdatalink.get("FRED/DSPIC96").reset_index().tail(40).rename(columns={'Value':'Real_Dispose'})
-    df = cpi.merge(unemployment, how='outer', on='Date').merge(real_disposable, how='outer', on='Date')
-    df.to_csv('/Users/Sam/01-Economics/CSV Files/Macro_Vars.csv')    
+    df = pd.DataFrame()
+    link = ['CPIAUCSL', 'UNRATE', 'DSPIC96']
+    for i in link:
+        data = pd.DataFrame(fred.get_series(i)).reset_index().tail(60)
+        data['Name'] = i
+        df = df.append(data)
+        
+    data = df.pivot(index = 'index', columns = 'Name', values = 0).reset_index().fillna(0)
+    data['Updated'] = pd.Timestamp.now()
+
+    data.to_csv('/Users/Sam/01-Economics/CSV Files/Macro_Vars.csv')    
     print(df)
     
     try:
@@ -30,3 +38,9 @@ git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE)
 #def job():
 #schedule.every().minute.do(job)
 
+
+
+#    cpi = nasdaqdatalink.get("FRED/CPIAUCSL").reset_index().tail(40).rename(columns={'Value':'CPI'})
+#    unemployment = nasdaqdatalink.get("FRED/UNRATE").reset_index().tail(40).rename(columns={'Value':'Unemployment'})
+#    real_disposable = nasdaqdatalink.get("FRED/DSPIC96").reset_index().tail(40).rename(columns={'Value':'Real_Dispose'})
+#    df = cpi.merge(unemployment, how='outer', on='Date').merge(real_disposable, how='outer', on='Date')
