e06d5f92e2ec231a01f449f62ae763d5cad9f427
musch038 <musch.sam@gmail.com>
2022-06-12 08:54:42 -0500

    Removed job sched

--- Testing2.py
+++ Testing2.py
@@ -1,50 +1,43 @@
-import schedule
-import time
 
-def job():
-    from git import Repo
-    import pandas as pd
+from git import Repo
+import pandas as pd
 
-    #import nasdaqdatalink
-    from fredapi import Fred
+#import nasdaqdatalink
+from fredapi import Fred
 
 
 
-    fred = Fred(api_key='07bccc6cd5cd1018027266ce854705db')
+fred = Fred(api_key='07bccc6cd5cd1018027266ce854705db')
 
-    PATH_OF_GIT_REPO = r'/Users/Sam/01-Economics'  # make sure .git folder is properly configured
-    COMMIT_MESSAGE = 'comment from python script'
+PATH_OF_GIT_REPO = r'/Users/Sam/01-Economics'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'comment from python script'
 
-    def git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE):
-        df = pd.DataFrame()
-        link = ['CPIAUCSL', 'UNRATE', 'DSPIC96']
-        for i in link:
-            data = pd.DataFrame(fred.get_series(i)).reset_index().tail(60)
-            data['Name'] = i
-            df = df.append(data)
-            
-        data = df.pivot(index = 'index', columns = 'Name', values = 0).reset_index().fillna(0)
-        data['Updated'] = pd.Timestamp.now()
-
-        data.to_csv('/Users/Sam/01-Economics/CSV Files/Macro_Vars.csv')    
-        print(df)
+def git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE):
+    df = pd.DataFrame()
+    link = ['CPIAUCSL', 'UNRATE', 'DSPIC96']
+    for i in link:
+        data = pd.DataFrame(fred.get_series(i)).reset_index().tail(60)
+        data['Name'] = i
+        df = df.append(data)
         
-        try:
-            repo = Repo(PATH_OF_GIT_REPO)
-            repo.git.add(all=True) # , update=True
-            repo.index.commit(COMMIT_MESSAGE)
-            origin = repo.remote(name='origin')
-            origin.push()
-        except:
-            print('Some error occured while pushing the code')    
+    data = df.pivot(index = 'index', columns = 'Name', values = 0).reset_index().fillna(0)
+    data['Updated'] = pd.Timestamp.now()
 
-    git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE)
-    schedule.every(2).minutes.do(job)
+    data.to_csv('/Users/Sam/01-Economics/CSV Files/Macro_Vars.csv')    
+    print(df)
+    
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(all=True) # , update=True
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')    
+
+git_push(PATH_OF_GIT_REPO, COMMIT_MESSAGE)
 
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
 
 
 
